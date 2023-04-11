de78d6a3d667f4c0209d9c246cb991e2756f81fa
Lavanya1176 <125252434+Lavanya1176@users.noreply.github.com>
2023-04-11 21:20:12 +0530

    Add files via upload

---
+++ AppointmentManager.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class AppointmentManager {
+
+    public void printVisitorList() {
+    }
+
+    public void addVisitor(Visitor visitor) {
+    }
+
+    public void editVisitor(String nameToEdit) {
+    }
+
+    public void printAppointmentSchedule(LocalDate date) {
+    }
+
+    public void bookAppointment(String visitorName, LocalDate appointmentDate, LocalTime appointmentTime) {
+    }
+
+}
